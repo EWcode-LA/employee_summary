b89ac439dbec0bf0b0546a3cfde9c6bee72803e0
EWcode-LA <seth.miller87@proton.mail>
2020-12-16 18:25:30 -0800

    initial

---
+++ Develop/lib/Employee.js
@@ -0,0 +1,19 @@
+// TODO: Write code to define and export the Employee class
+//Function that has employe name, position, id, email, and office number or github
+
+// function Employee(name, position, id, email, github) {
+//     this.name = 
+//     this.position =
+//     this.id =
+//     this.email =
+//     this. github =
+// }
+//create class per readme
+
+class Employee {
+    constructor(id, name, title) {
+        this.id = id;
+        this.name = name;
+        this.title = title;
+    }
+}
